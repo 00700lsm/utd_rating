a80cf2ea70343026fe63e3c933235a11a8c721b8
00700lsm <00700lsm@gmail.com>
2024-10-04 21:56:36 +0900

    맨유 평점 프로그램 초기화

---
+++ src/main/kotlin/com/lsm/utdrating/UtdratingApplication.kt
@@ -0,0 +1,11 @@
+package com.lsm.utdrating
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UtdratingApplication
+
+fun main(args: Array<String>) {
+	runApplication<UtdratingApplication>(*args)
+}
---
+++ src/test/kotlin/com/lsm/utdrating/UtdratingApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.lsm.utdrating
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UtdratingApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
